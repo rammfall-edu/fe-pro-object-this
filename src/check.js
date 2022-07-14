661bf79a48faac6f0f2bcac917931782162d1e69
oleg-boyko <97238155+oleg-boyko@users.noreply.github.com>
2022-07-14 15:35:29 +0300

    Update check.js

--- src/check.js
+++ src/check.js
@@ -1,1 +1,53 @@
 import { hotel } from './index.js';
+  export const hotel =  {
+  quantityOfPlaces: 30,
+  priceByPlace: 20, 
+  bankAccount: 0,
+  guests: {
+  0: {
+   firstName: Oleg, 
+   lastName: Boyko,
+   money: 40,
+     },
+  1: {
+    firstName: Ivan, 
+    lastName: Ivanov,
+    money: 40,
+     } 
+     }, // end guests
+  
+  getLength: function() {
+  return Object.keys(this.guests).length
+  },
+  
+   getActualFreePlace:  function() {
+   return  this.quantityOfPlaces - this.getLength()
+  },
+  
+   paidPerPlace: function() {
+   return this.bankAccount = this.bankAccount + this.priceByPlace
+   },
+ 
+   checkInGuest: function(name, lastName, money) {
+   if (this.getActualFreePlace === 0){
+   return 'Sorry, we have not free spaces'
+   }
+    if (this.priceByPlace > money){
+     return 'Sorry, you have not enough money'
+    }
+     
+   const user = {
+   firstName: name,
+   lastName,
+    money: money - this.priceByPlace
+   }
+   
+   this.guest [this.getLength()] = user;
+    
+    this.paidPerPlace();
+    
+    return user;
+    
+   }, //end checkinGuest
+  
+};
