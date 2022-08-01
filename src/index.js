2c611b2e391f8792475d7b57725494da585d090a
AndreyPodvigin <sn.podvigin.andrey@gmail.com>
2022-08-01 13:22:43 +0300

    first attempt

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-export const hotel = new Object();
+export const hotel = {
+  quantityOfPlaces: 30,
+  priceByPlace: 20,
+
+  bankAccount: 0,
+  guests: {},
+
+  getLength(){
+    const objectInArray = Object.keys(this.guests);
+    return objectInArray.length;
+  },
+
+  getActualFreePlace(){
+    return this.quantityOfPlaces - this.getLength();
+  },
+
+  paidPerPlace(){
+    this.bankAccount += this.priceByPlace;
+  },
+  
+  checkInGuest(firstName, lastName, avaiilableBalance){
+    if (this.getLength() === this.quantityOfPlaces) {
+      return 'Sorry, we have not free spaces'
+    } else if (avaiilableBalance < this.priceByPlace) {
+      return 'Sorry, you have not enough money'
+    } else {
+      this.paidPerPlace();
+      this.guests[this.getLength()] = {
+        firstName,
+        lastName,
+        money: avaiilableBalance - this.priceByPlace,
+      }
+    }
+  },
+};
